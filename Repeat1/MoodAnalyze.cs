b9360bbf80601f6eb2a98c5c501bf32926cd445f
chai2222 <chaitanayajagadale6@gmail.com>
2023-01-20 21:39:15 +0530

    CheckFlow

---
+++ Repeat1/MoodAnalyze.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repeat1
+{
+    public class MoodAnalyser
+    {
+        //Refactor to take message in Constructor
+        public string message;
+        public MoodAnalyser()
+        {
+
+        }
+
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyseMood()
+        {
+            if (message.ToUpper().Contains("SAD"))
+            {
+                return "Sad Mood";
+            }
+            else
+            {
+                return "Happy Mood";
+            }
+        }
+    }
+}
---
+++ Repeat1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Repeat1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser Program");
+
+
+            Console.WriteLine("Enter your Mood:");
+            string message = Console.ReadLine();
+            //Creating object of MoodAnalyser class
+
+            MoodAnalyser moodobj = new MoodAnalyser(message);
+
+
+            Console.WriteLine(moodobj.AnalyseMood());
+        }
+    }
+}
